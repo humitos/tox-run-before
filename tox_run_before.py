474a3c59a34613ec8e84956e422de211f41e227b
Manuel Kaufmann <humitos@gmail.com>
2025-02-24 13:56:55 +0100

    Update tox_run_before.py

--- tox_run_before.py
+++ tox_run_before.py
@@ -4,7 +4,7 @@ from os import system
 hookimpl = pluggy.HookimplMarker("tox")
 
 @hookimpl
-def tox_configure(config):
+def tox_before_run_commands(config):
     for env in config.envlist:
         for cmd in config.envconfigs[env]._reader.getlist("run_before"):
             system(cmd)
